a6763ff80da04895e10203f970e27dc8ce1b169f
KrishnaNayak007 <krrishnayak144@gmail.com>
2026-06-25 16:37:19 +0530

    Configure absolute virtualenv paths in build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -2,9 +2,9 @@
 # exit on error
 set -o errexit
 
-# Force pip to install everything inside the Poetry virtualenv
-poetry run pip install -r requirements.txt
+# Install requirements directly into the active virtual environment created by Render (located one directory up)
+/opt/render/project/src/.venv/bin/pip install -r requirements.txt
 
-# Run Django commands inside the Poetry virtualenv
-poetry run python manage.py collectstatic --no-input
-poetry run python manage.py migrate
+# Run Django commands using the virtual environment's python interpreter
+/opt/render/project/src/.venv/bin/python manage.py collectstatic --no-input
+/opt/render/project/src/.venv/bin/python manage.py migrate
